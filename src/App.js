2a10b5a9439c2812721eb4c610804363730bd39d
Glyan <pamela27jr@gmail.com>
2022-01-18 17:42:58 +0000

    Adding lazy queries

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CharactersList from './pages/CharactersList';
 import Character from './pages/Character';
 import { Route, Routes } from "react-router-dom";
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route stric exact path="/" element={<CharactersList/>} />
+        <Route stric exact path="/search" element={<Search/>} />
         <Route stric exact path="/:id" element={<Character/>} />
       </Routes>
     </div>
